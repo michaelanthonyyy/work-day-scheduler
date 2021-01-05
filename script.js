c61ea44266a11c50a602fed1e56db47a0b663096
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-05 15:13:39 -0800

    commented outline for script.js file to be written

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
+
+// current date and time header
 $("#currentDate h5").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+// define work hours as variable?
+
+
+
+
+// on click function for save buttons. sets to save to specific location in local storage
+$("button").click(function(){
+    value = "";
+})
+
+
+// code to write texts to local storage after hitting save
+$(document).ready(function(){
+    if(localStorage.getItem()) {
+        changeSchedule();
+    }
+})
+
+
+// track current hours, past hours, and later hours 
+// past hours boxes darkened, current hour changed to red, future hours lime green
+
+
+
+
 
