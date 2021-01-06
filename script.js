1ebfc6d425a50123431487a362e029661ac8d586
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-01-05 16:38:21 -0800

    Added local storage save on button click. Text area loaded on refresh page for each hour

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-
-// current date and time header
+// Current date and time header
 $("#currentDate h5").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 
-// define work hours as variable?
+// Loads local storage into textarea for each hour on the schedule
+$(".nine").text(localStorage.getItem("9 AM"));
+$(".ten").text(localStorage.getItem("10 AM"));
+$(".eleven").text(localStorage.getItem("11 AM"));
+$(".twelve").text(localStorage.getItem("12 PM"));
+$(".one").text(localStorage.getItem("1 PM"));
+$(".two").text(localStorage.getItem("2 PM"));
+$(".three").text(localStorage.getItem("3 PM"));
+$(".four").text(localStorage.getItem("4 PM"));
+$(".five").text(localStorage.getItem("5 PM"));
 
 
-
-
-// on click function for save buttons. sets to save to specific location in local storage
-$("button").click(function(){
-    value = "";
+// Button on click function to save the textarea value into local storage for each
+// specified hour of the day
+$("button").click(function () {
+    var value = $(this).siblings(".textentry").val();
+    var key = $(this).siblings(".time").text();
+    localStorage.setItem(key, value);
+    // console.log(key);
+    // console.log(value);
 })
-
-
-// code to write texts to local storage after hitting save
-$(document).ready(function(){
-    if(localStorage.getItem()) {
-        changeSchedule();
-    }
-})
-
 
 // track current hours, past hours, and later hours 
 // past hours boxes darkened, current hour changed to red, future hours lime green
